ef448e7426a28400fd9ae4dde0e2185f99546bf1
alshabehera <alshabehera7@gmail.com>
2025-03-17 08:25:48 +0530

    Added validation to Post model

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum:125 }
+  validates :description, presence: true, length: { maximum:10_000 }
+  validates_inclusion_of :is_bloggable, in: [true, false]
+
+end
