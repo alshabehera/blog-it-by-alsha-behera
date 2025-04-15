7ef73ca6b9fe3fa67493304c699e74a6c4795e88
alshabehera <alshabehera7@gmail.com>
2025-04-15 14:29:38 +0530

    Made name attribute not nullable in category

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_03_085006) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_15_085810) do
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
